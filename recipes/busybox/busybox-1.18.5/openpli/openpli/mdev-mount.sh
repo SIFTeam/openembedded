ef1baa3536fef44cb2e81301a1f4785b198cdb21
MiLo <milo-software@users.sourceforge.net>
2011-09-14 20:15:49 +0200

    Introduce busybox 1.18.5

---
+++ recipes/busybox/busybox-1.18.5/openpli/openpli/mdev-mount.sh
@@ -0,0 +1,132 @@
+#!/bin/sh
+
+MOUNTPOINT="/media/$MDEV"
+NOTIFYDEVNAME=$MDEV
+
+notify() {
+	# we don't really depend on the hotplug_e2_helper, but when it exists, call it
+	if [ -x /usr/bin/hotplug_e2_helper ]; then
+		/usr/bin/hotplug_e2_helper $ACTION /block/$MDEV $PHYSDEVPATH
+	fi
+}
+
+case "$ACTION" in
+	add|"")
+		ACTION="add"
+		# check if already mounted
+		if grep -q "^/dev/${MDEV} " /proc/mounts ; then
+			# Already mounted
+			exit 0
+		fi
+		# remove old mountpoint symlinks we might have for this device
+		rm -f $MOUNTPOINT
+		DEVBASE=`expr substr $MDEV 1 3`
+		# check for "please don't mount it" file
+		if [ -f "/dev/nomount.${DEVBASE}" ] ; then
+			# blocked
+			exit 0
+		fi
+		# check for full-disk partition
+		if [ "${DEVBASE}" == "${MDEV}" ] ; then
+			if [ -d /sys/block/${DEVBASE}/${DEVBASE}1 ] ; then
+				# Partition detected, just tell and quit
+				notify
+				exit 0
+			fi
+			if [ ! -f /sys/block/${DEVBASE}/size ] ; then
+				# No size at all
+				exit 0
+			fi
+			if [ `cat /sys/block/${DEVBASE}/size` == 0 ] ; then
+				# empty device, bail out
+				exit 0
+			fi
+		fi
+		# first allow fstab to determine the mountpoint
+		mount /dev/$MDEV
+		if [ $? -ne 0 ] ; then
+			# no fstab entry, use automatic mountpoint
+			REMOVABLE=`cat /sys/block/$DEVBASE/removable`
+			MODEL=`cat /sys/block/$DEVBASE/device/model`
+			if [ "${REMOVABLE}" -eq "0" ]; then
+				# mount the first non-removable device on /media/hdd
+				DEVICETYPE="hdd"
+			else
+				if [ "$MODEL" == "USB CF Reader   " ]; then
+					DEVICETYPE="cf"
+				elif [ "$MODEL" == "Compact Flash   " ]; then
+					DEVICETYPE="cf"
+				elif [ "$MODEL" == "USB SD Reader   " ]; then
+					DEVICETYPE="mmc1"
+				elif [ "$MODEL" == "SD/MMC          " ]; then
+					DEVICETYPE="mmc1"
+				elif [ "$MODEL" == "USB MS Reader   " ]; then
+					DEVICETYPE="mmc1"
+				elif [ "$MODEL" == "SM/xD-Picture   " ]; then
+					DEVICETYPE="mmc1"
+				elif [ "$MODEL" == "USB SM Reader   " ]; then
+					DEVICETYPE="mmc1"
+				elif [ "$MODEL" == "MS/MS-Pro       " ]; then
+					DEVICETYPE="mmc1"
+				else
+					DEVICETYPE="usb"
+				fi
+			fi
+			touch /dev/mdev.$DEVICETYPE
+			DEVSTATE=`cat /dev/mdev.$DEVICETYPE`
+			if [ -z $DEVSTATE ]; then
+				MOUNTPOINT="/media/$DEVICETYPE"
+				NOTIFYDEVNAME=$DEVICETYPE
+				echo $MDEV > /dev/mdev.$DEVICETYPE
+			fi
+			mkdir -p $MOUNTPOINT
+			mount -t auto /dev/$MDEV $MOUNTPOINT
+			rm -f /autofs/$MDEV
+			ln -s $MOUNTPOINT /autofs/$MDEV
+		else
+			# we used an fstab entry, try to find out what happened
+			finddevice ()
+			{
+				DEVICETYPE="$1"
+				cat /proc/mounts | grep /media/$DEVICETYPE | grep /dev/$MDEV
+				if [ $? -eq 0 ]; then
+					# we've just mounted our device on /media/$DEVICETYPE
+					NOTIFYDEVNAME=$DEVICETYPE
+					echo $MDEV > /dev/mdev.$DEVICETYPE
+				fi
+			}
+			finddevice "hdd"
+			finddevice "usb"
+			finddevice "cf"
+			finddevice "mmc1"
+		fi
+		;;
+	remove)
+		umount /dev/$MDEV
+		finddevice ()
+		{
+			DEVICETYPE="$1"
+			touch /dev/mdev.$DEVICETYPE
+			DEVSTATE=`cat /dev/mdev.$DEVICETYPE`
+			if [ "${DEVSTATE}" == "${MDEV}" ]; then
+				rm /dev/mdev.$DEVICETYPE
+				NOTIFYDEVNAME=$DEVICETYPE
+			fi
+		}
+		finddevice "hdd"
+		finddevice "usb"
+		finddevice "cf"
+		finddevice "mmc1"
+		# remove automatic mountpoint (if not empty...)
+		rmdir $MOUNTPOINT
+		# in case it was a symlink, remove that as well
+		rm -f $MOUNTPOINT
+		rm -f /autofs/$MDEV
+		;;
+	*)
+		# Unexpected keyword
+		exit 1
+		;;
+esac
+
+notify
