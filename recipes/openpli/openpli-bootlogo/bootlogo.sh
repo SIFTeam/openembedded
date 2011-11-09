473893e7b3d9b3c5011dc60fea5cd798b5614c34
pieterg <pieterg@users.sourceforge.net>
2011-11-09 09:38:51 +0100

    openpli-bootlogo: ignore /etc/videomode when it does not exist

--- recipes/openpli/openpli-bootlogo/bootlogo.sh
+++ recipes/openpli/openpli-bootlogo/bootlogo.sh
@@ -1,6 +1,6 @@
 # avoid the console messages clobbering our logo
 [ -f /sys/class/vtconsole/vtcon1/bind ] && echo 0 > /sys/class/vtconsole/vtcon1/bind
 # and set the correct videomode before showing the bootlogo
-cat /etc/videomode > /proc/stb/video/videomode
+[ -f /etc/videomode ] && cat /etc/videomode > /proc/stb/video/videomode
 
 /usr/bin/showiframe /usr/share/bootlogo.mvi
