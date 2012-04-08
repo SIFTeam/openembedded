d33b7e2ea6b4b86585cc3c454c004ae981a1e14a
MiLo <milo-software@users.sourceforge.net>
2012-04-08 14:40:37 +0200

    base-files: Make /media a volatile filesystem
    /media is written to by all kinds of scripts (mdev-mount, enigma2)
    but there's nothing persistent here. So it's better to put it in
    a volatile location:
    - No needless writing to flash
    - If you do something stupid, you just fill 64k RAM and then get an
      error. That's better than killing your box by filling the rootfs.
    - mdev-mount.sh can use /media as administration area because it does
      not survive reboots any more, so the "/dev/mdev.hdd" and similar
      administration files can be removed from the script.
    
    Mounted as a separate tmpfs because:
    - We can't use volatiles because we need to start before mdev.
    - Scripts that do "os.path.realpath(x).startswith('/media')" would no
      longer work as expected when /media is a symlink.
    - It allows putting a tight limit on the RAM usage.
    
    At startup, volatile-media.sh mounts /media and parses /etc/fstab
    to populate it, so that user-defined mountpoints continue to work.

---
+++ recipes/openpli/volatile-media/volatile-media.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+mount -t tmpfs -o size=64k tmpfs /media
+
+create_point() {
+	if [ ! -d $2 ]
+	then
+		mkdir -p $2
+	fi
+}
+
+grep -v "^#" /etc/fstab | while read LINE
+do
+	create_point $LINE
+done
